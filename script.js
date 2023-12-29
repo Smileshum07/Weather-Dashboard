02e2f72a70c90ae30f414d92557a566c52fe0621
Anna <103360175+Smileshum07@users.noreply.github.com>
2023-12-29 00:51:03 +0000

    Forecast is added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${getLat()}&lon=${lon}&appid=${APIKey}`;
 var APIKey = '0dc2e4aef9c53ae1be98352041f29566';
 var searchButton = document.getElementById('search-button');
 var input = document.getElementById('search-input');
@@ -19,15 +18,17 @@ function clickSearchButton(e) {
     localStorage.setItem(searchButton, name);
     var localCityName = document.createElement('button');
     localCityName.textContent = localStorage.getItem(`${localCityName}`);
+    localCityName.setAttribute('class', 'show')
     history.appendChild(localCityName);
     
     // get the lon, lat 
-    queryUrlGet = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${APIKey}`
-    fetch(queryUrlGet)
-        .then(function (response) {
+    
+    function getData() {
+        queryUrlGet = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${APIKey}`;
+        fetch(queryUrlGet).then(function (response) {
             return response.json();
         }).then(function (data) {
-            console.log(data)
+            // console.log(data)
             var lat = data[0].lat;
             console.log(lat)
             var lon = data[0].lon;
@@ -62,11 +63,40 @@ function clickSearchButton(e) {
                     var humid = document.getElementById('humid-today');
                     humid.textContent = `Humidity: ${weatherArrey[0].main.humidity} %`
                      
-           
+                    // get forecast
+                    
+                    var forecastTitle = document.getElementById('forecastTitle');
+                    // console.log(forecastTitle)
+                    forecastTitle.textContent = '5-Day Forecast';
+                    for (var i = 6; i < weatherArrey.length; i += 7) {
+                        var forecastCars = document.getElementById('forecast-cars');
+                        var date = dayjs(`${weatherArrey[i].dt_txt}`).format('DD.MM.YYYY');
+                        var srcForecast = `${weatherArrey[i].weather[0].icon}.png`;
+                        var cardForecast = `
+                                    <div class="cards me-5 bg-primary p-4 rounded-2" style="width: 100%;">
+                                        <div class="row">
+                                        <p class="date fs-5 fw-semibold text-white">${date}</p>
+                                        <img class="icon" src='./img/${srcForecast}' style="width: 65px;"></img>
+                                        </div>
+                                        <div class="tempr text-white">Temp: ${Math.round(weatherArrey[i].main.temp - 273, 15)}°C</div>
+                                        <div class="wind text-white">Wind: ${weatherArrey[i].wind.speed} KPH</div>
+                                        <div class="humid text-white">Humidity: ${weatherArrey[i].main.humidity} %</div>
+                                    </div>
+                                    `;
+                        var cardElement = document.createElement('div');
+                        cardElement.innerHTML = cardForecast;
+                        forecastCars.appendChild(cardElement);
+                    };
                 });
    
 
         })
-};
+    };
+    getData();
+}
+
+// window.addEventListener('load', function () {
+//     if(localStorage.getItem(''))
+// })
 
 
